cc10bda726d133de9140566c89d11c1254041105
Shanyou Li <shanyouli6@gmail.com>
2023-11-24 23:07:51 +0800

    A simpler main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,14 @@
+#!/usr/bin/env bash
 base_url="https://github.com/OwO-Network/DeepLX"
 
 update=0
-
+last_version=$(curl https://api.github.com/repos/OwO-Network/DeepLX/releases/latest | jq .name)
 if [[ ! -f ./version ]] || [[ ! -f ./deeplx ]]; then
     update=1
-    last_version=$(curl https://api.github.com/repos/OwO-Network/DeepLX/releases/latest | jq .name)
-else
-    if [[ -x ./deeplx ]]; then
-        last_version=$(curl https://api.github.com/repos/OwO-Network/DeepLX/releases/latest | jq .name)
-        if [[ $last_version != $(cat ./version) ]]; then
-            update=1
-        fi
-    fi
+elif [[ $last_version != $(cat ./version) ]]; then
+    update=1
 fi
 
-echo $last_version
 if [[ $update == 1 ]]; then
     wget $base_url/releases/download/${last_version//\"/}/deeplx_linux_amd64 -O deeplx
     chmod +x ./deeplx
